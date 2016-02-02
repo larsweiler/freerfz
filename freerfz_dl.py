0802802a60206b390477d1b2ae4ed0b8fce7431d
Lars Weiler <dc4lw@darc.de>
2016-02-02 21:49:31 +0100

    Prüft, ob Download notwendig ist; Umstellung auf Klasse E Rufzeichen für schnellere Analyse.

--- freerfz_dl.py
+++ freerfz_dl.py
@@ -30,18 +30,57 @@ import subprocess
 import shlex
 import string
 import re
+import urllib2
 
 class DLCalls:
     def __init__(self):
         # regex for Klasse A calls
         self.acalls = "D([CDGHJ][0-9]|[BFKLM][1-9])[A-Z]{2,3}"
-        pdfgrepcall = "/usr/local/bin/pdfgrep -o \""+self.acalls+"\" Rufzeichenliste_AFU.pdf"
-        args = shlex.split(pdfgrepcall)
-        proc = subprocess.Popen(args, stdout=subprocess.PIPE, shell=False)
-        (out, err) = proc.communicate()
+        self.ecalls = "DO[1-9][A-Z]{2,3}"
+        self.pdffile = 'Rufzeichenliste_AFU.pdf'
+        self.outfile = 'freecalls.txt'
+        self.download_Rufzeichenliste()
+        pdfgrepcall = "/usr/local/bin/pdfgrep -o \""+self.ecalls+"\" "+self.pdffile
+        try:
+            proc = subprocess.Popen(shlex.split(pdfgrepcall), stdout=subprocess.PIPE, shell=False)
+            (out, err) = proc.communicate()
+        except subprocess.CalledProcessError as e:
+            print "Error bei pdfgrep. Beende Programm."
+            print e.output
         self.dlcalls = out.split()
+
     def out(self):
         print self.dlcalls
+        return
+
+    def download_Rufzeichenliste(self):
+        url = 'http://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Telekommunikation/Unternehmen_Institutionen/Frequenzen/Amateurfunk/Rufzeichenliste/Rufzeichenliste_AFU.pdf?__blob=publicationFile&v=11'
+        dl = False
+
+        r = urllib2.urlopen(url)
+        downloadlength = r.headers['content-length']
+
+        try:
+            fd = open(self.pdffile, 'rb')
+            filelength = len(fd.read())
+            fd.close()
+            if int(downloadlength) != int(filelength):
+                dl = True
+        except IOError:
+            dl = True
+
+        if dl:
+            print "Lade neues PDF runter"
+            try:
+                with open(self.pdffile, 'wb') as f:
+                    f.write(r.read())
+            except:
+                print "Download Error"
+        else:
+            print "Rufzeichenliste braucht nicht erneut runter geladen zu werden."
+        r.close()
+        return
+
     def nonqgroup(self):
         q = []
         # no Q-Groups from QOA-QUZ allowed as suffix
@@ -49,13 +88,15 @@ class DLCalls:
             for b in string.ascii_uppercase:
                 q.append("Q"+a+b)
         return q
+
     def freecalls(self):
-        pattern = re.compile(self.acalls)
+        pattern = re.compile(self.ecalls)
         # these suffixes are not allowed
         nonsuffix = ["SOS", "XXX", "TTT", "YYY", "DDD", "JJJ", "MAYDAY", "PAN"] + self.nonqgroup()
-        print nonsuffix
-        f = open('freecalls.txt', 'w')
-        for prefix in ['DB', 'DC', 'DD', 'DF', 'DG', 'DH', 'DJ', 'DK', 'DL', 'DM']:
+        f = open(self.outfile, 'w')
+        #for prefix in ['DB', 'DC', 'DD', 'DF', 'DG', 'DH', 'DJ', 'DK', 'DL', 'DM']:
+        print "Generiere freie Klasse E Rufzeichen"
+        for prefix in ['DO']:
             for number in range(10):
                 for alpha in string.ascii_uppercase:
                     for bravo in string.ascii_uppercase:
@@ -72,6 +113,8 @@ class DLCalls:
                                 if call not in self.dlcalls:
                                     f.write("%s\n" % call)
         f.close()
+        print "Freie Rufzeichen liegen in der Datei '%s'." % (self.freecalls)
+        return
 
 
 if __name__ == "__main__":
